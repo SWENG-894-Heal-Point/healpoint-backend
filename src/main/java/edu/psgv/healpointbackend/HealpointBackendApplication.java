e6af95f26101eefccdf117451cfa2612c6d57241
Mahfuzur Rahman <mvr5854@psu.edu>
2025-09-11 22:44:59 -0400

    first commit

---
+++ src/main/java/edu/psgv/healpointbackend/HealpointBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.psgv.healpointbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealpointBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealpointBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/psgv/healpointbackend/HealpointBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.psgv.healpointbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealpointBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
