4317de888789b781333830ca708ddb58262eba58
Mahfuzur Rahman <mvr5854@psu.edu>
2025-09-20 11:57:55 -0400

    Implement IoHelper for string validation and PasswordUtils for password hashing

--- src/main/java/edu/psgv/healpointbackend/HealpointBackendApplication.java
+++ src/main/java/edu/psgv/healpointbackend/HealpointBackendApplication.java
@@ -8,9 +8,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
